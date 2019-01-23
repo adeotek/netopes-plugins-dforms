a83c1f9c0f96c2972f44bf994d7de9bcfb863f8e
George Benjamin-Schonberger <george.benjamin@gmail.com>
2019-01-23 22:42:05 +0200

    CHANGE - ValuesLists data source constants added

--- DataSources/ValuesLists.php
+++ DataSources/ValuesLists.php
@@ -11,7 +11,6 @@
  */
 namespace NETopes\Plugins\DataSources\DForms;
 use NETopes\Core\Data\DataSource;
-
 /**
  * ValuesLists data source class
  * For all database call methods the default parameters configuration is:
@@ -30,40 +29,94 @@ use NETopes\Core\Data\DataSource;
  * @package  NETopes\Plugins\DataSources\DForms
  */
 abstract class ValuesLists extends DataSource {
-	/**
-	 * Gets forms values lists
-	 */
+    /**
+     * GetItems default parameters
+     */
+    const GET_ITEMS = ['for_id'=>NULL,'for_state'=>NULL,'for_text'=>NULL];
+    /**
+     * Gets forms values lists
+     * @param array $params ['for_id'=>NULL,'for_state'=>NULL,'for_text'=>NULL]
+     * @param array $extra_params
+     */
 	abstract public function GetItems($params = [],$extra_params = []);
+    /**
+     * SetNewItem default parameters
+     */
+    const SET_NEW_ITEM = ['in_ltype'=>NULL,'in_name'=>NULL,'in_state'=>NULL];
 	/**
 	 * Sets a new values list
+     * @param array $params ['in_ltype'=>NULL,'in_name'=>NULL,'in_state'=>NULL]
+     * @param array $extra_params
 	 */
 	abstract public function SetNewItem($params = [],$extra_params = []);
+    /**
+     * SetItem default parameters
+     */
+    const SET_ITEM = ['for_id'=>NULL,'in_name'=>NULL,'in_state'=>NULL];
 	/**
 	 * Sets a values list
+     * @param array $params ['for_id'=>NULL,'in_name'=>NULL,'in_state'=>NULL]
+     * @param array $extra_params
 	 */
 	abstract public function SetItem($params = [],$extra_params = []);
+    /**
+     * UnsetItem default parameters
+     */
+    const UNSET_ITEM = ['for_id'=>NULL];
 	/**
 	 * Unets a values list
+     * @param array $params ['for_id'=>NULL]
+     * @param array $extra_params
 	 */
 	abstract public function UnsetItem($params = [],$extra_params = []);
+    /**
+     * GetValues default parameters
+     */
+    const GET_VALUES = ['for_id'=>NULL,'list_id'=>NULL,'for_ltype'=>NULL,'for_state'=>NULL,'for_implicit'=>NULL,'for_text'=>NULL];
 	/**
 	 * Gets forms lists values
+     * @param array $params ['for_id'=>NULL,'list_id'=>NULL,'for_ltype'=>NULL,'for_state'=>NULL,'for_implicit'=>NULL,'for_text'=>NULL]
+     * @param array $extra_params
 	 */
 	abstract public function GetValues($params = [],$extra_params = []);
+    /**
+     * GetValueItems default parameters
+     */
+    const GET_VALUE_ITEMS = ['for_id'=>NULL,'list_id'=>NULL,'for_ltype'=>NULL,'for_state'=>NULL];
 	/**
 	 * Gets forms lists values items
+     * @param array $params ['for_id'=>NULL,'list_id'=>NULL,'for_ltype'=>NULL,'for_state'=>NULL]
+     * @param array $extra_params
 	 */
 	abstract public function GetValueItems($params = [],$extra_params = []);
+    /**
+     * SetNewValue default parameters
+     */
+    const SET_NEW_VALUE = ['list_id'=>NULL,'in_value'=>NULL,'in_name'=>NULL,'in_state'=>NULL,'in_implicit'=>NULL];
 	/**
 	 * Sets a new list value
+     * @param array $params ['list_id'=>NULL,'in_value'=>NULL,'in_name'=>NULL,'in_state'=>NULL,'in_implicit'=>NULL]
+     * @param array $extra_params
 	 */
 	abstract public function SetNewValue($params = [],$extra_params = []);
+    /**
+     * SetValue default parameters
+     */
+    const SET_VALUE = ['for_id'=>NULL,'in_value'=>NULL,'in_name'=>NULL,'in_state'=>NULL,'in_implicit'=>NULL];
 	/**
 	 * Sets a list value
+     * @param array $params ['for_id'=>NULL,'in_value'=>NULL,'in_name'=>NULL,'in_state'=>NULL,'in_implicit'=>NULL]
+     * @param array $extra_params
 	 */
 	abstract public function SetValue($params = [],$extra_params = []);
+    /**
+     * UnsetValue default parameters
+     */
+    const UNSET_VALUE = ['for_id'=>NULL];
 	/**
 	 * Unets a list value
+     * @param array $params ['for_id'=>NULL]
+     * @param array $extra_params
 	 */
 	abstract public function UnsetValue($params = [],$extra_params = []);
 }//END class ValuesLists extends DataSource
