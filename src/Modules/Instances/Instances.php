be861a11b9b622832bf99cc926880c0a7f9fab4c
George Benjamin-Schonberger <george.benjamin@gmail.com>
2019-05-07 22:32:12 +0300

    FIX - Fixes and optimizations

--- src/Modules/Instances/Instances.php
+++ src/Modules/Instances/Instances.php
@@ -457,12 +457,14 @@ class Instances extends Module {
             $view->AddHtmlContent('<div class="row"><div class="col-md-12 clsBasicFormErrMsg" id="'.$fResponseTarget.'">&nbsp;</div></div>');
             $btnSave=new Button(['value'=>Translate::GetButton('save'),'icon'=>'fa fa-save','class'=>NApp::$theme->GetBtnPrimaryClass(),'onclick'=>NApp::Ajax()->Prepare("{ 'module': '{$this->class}', 'method': 'SaveInstance', 'params': { 'id_template': {$idTemplate}, 'id': {$idInstance}, 'data': '{nGet|df_{$tName}_form:form}', 'is_modal': '{$isModal}', 'cmodule': '{$cModule}', 'cmethod': '{$cMethod}', 'ctarget': '{$cTarget}', 'target': '{$fTagId}' } }",$fResponseTarget)]);
             $view->AddAction($btnSave->Show());
-            if($isModal) {
-                $btnBack=new Button(['value'=>Translate::GetButton('cancel'),'class'=>NApp::$theme->GetBtnDefaultClass(),'icon'=>'fa fa-ban','onclick'=>"CloseModalForm()",]);
-            } else {
-                $btnBack=new Button(['value'=>Translate::GetButton('back'),'icon'=>'fa fa-chevron-left','class'=>NApp::$theme->GetBtnDefaultClass(),'onclick'=>NApp::Ajax()->Prepare("{ 'module': '{$cModule}', 'method': '{$cMethod}', 'params': { 'id_template': {$idTemplate}, 'id': {$idInstance}, 'target': '{$cTarget}' } }",$cTarget)]);
-            }//if($isModal)
-            $view->AddAction($btnBack->Show());
+            if($params->safeGet('back_action',TRUE,'bool')) {
+                if($isModal) {
+                    $btnBack=new Button(['tag_id'=>'df_'.$tName.'_cancel','value'=>Translate::GetButton('cancel'),'class'=>NApp::$theme->GetBtnDefaultClass(),'icon'=>'fa fa-ban','onclick'=>"CloseModalForm()",]);
+                } else {
+                    $btnBack=new Button(['tag_id'=>'df_'.$tName.'_back','value'=>Translate::GetButton('back'),'icon'=>'fa fa-chevron-left','class'=>NApp::$theme->GetBtnDefaultClass(),'onclick'=>NApp::Ajax()->Prepare("{ 'module': '{$cModule}', 'method': '{$cMethod}', 'params': { 'id_template': {$idTemplate}, 'id': {$idInstance}, 'target': '{$cTarget}' } }",$cTarget)]);
+                }//if($isModal)
+                $view->AddAction($btnBack->Show());
+            }//if($params->safeGet('back_action',TRUE,'bool'))
         }//if(strlen($fTagId))
         $addContentMethod='Add'.$controlClass;
         $view->$addContentMethod($this->GetViewFile('AddEditInstanceForm'));
--- src/Modules/Instances/_default/AddEditInstanceForm.php
+++ src/Modules/Instances/_default/AddEditInstanceForm.php
@@ -1,8 +1,13 @@
 <?php
 /**
- * @var string $controlClass
- * @var string $tName
- * @var string $fTagId
+ * @var string      $controlClass
+ * @var string      $tName
+ * @var string      $fTagId
+ * @var int         $idTemplate
+ * @var int         $idInstance
+ * @var string|null $cModule
+ * @var string|null $cMethod
+ * @var string|null $cTarget
  */
 
 if($controlClass=='BasicForm' && isset($ctrl_params) && is_array($ctrl_params)) {
@@ -13,15 +18,17 @@ if($controlClass=='BasicForm' && isset($ctrl_params) && is_array($ctrl_params)) 
                 'params'=>['tag_id'=>'df_'.$tName.'_save','value'=>Translate::GetButton('save'),'icon'=>'fa fa-save','class'=>NApp::$theme->GetBtnPrimaryClass(),'onclick'=>NApp::Ajax()->LegacyPrepare("AjaxRequest('{$this->class}','SaveRecord','id_template'|{$idTemplate}~'id'|{$idInstance}~'data'|df_{$tName}_form:form~'is_modal'|'{$isModal}'~'cmodule'|'{$cModule}'~'cmethod'|'{$cMethod}'~'ctarget'|'{$cTarget}','{$fTagId}')->{$fResponseTarget}")],
             ],
         ];
-        if($isModal) {
-            $ctrl_params['actions'][]=[
-                'type'=>'CloseModal',
-                'params'=>['tag_id'=>'df_'.$tName.'_cancel','value'=>Translate::GetButton('cancel'),'class'=>NApp::$theme->GetBtnDefaultClass(),'icon'=>'fa fa-ban'],
-            ];
-        } else {
-            $ctrl_params['actions'][]=[
-                'params'=>['tag_id'=>'df_'.$tName.'_back','value'=>Translate::GetButton('back'),'icon'=>'fa fa-chevron-left','class'=>NApp::$theme->GetBtnDefaultClass(),'onclick'=>NApp::Ajax()->LegacyPrepare("AjaxRequest('{$cModule}','{$cMethod}','id_template'|{$idTemplate}~'id'|{$idInstance},'{$cTarget}')->{$cTarget}")],
-            ];
-        }//if($isModal)
+        if($params->safeGet('back_action',TRUE,'bool')) {
+            if($isModal) {
+                $ctrl_params['actions'][]=[
+                    'type'=>'CloseModal',
+                    'params'=>['tag_id'=>'df_'.$tName.'_cancel','value'=>Translate::GetButton('cancel'),'class'=>NApp::$theme->GetBtnDefaultClass(),'icon'=>'fa fa-ban'],
+                ];
+            } else {
+                $ctrl_params['actions'][]=[
+                    'params'=>['tag_id'=>'df_'.$tName.'_back','value'=>Translate::GetButton('back'),'icon'=>'fa fa-chevron-left','class'=>NApp::$theme->GetBtnDefaultClass(),'onclick'=>NApp::Ajax()->LegacyPrepare("AjaxRequest('{$cModule}','{$cMethod}','id_template'|{$idTemplate}~'id'|{$idInstance},'{$cTarget}')->{$cTarget}")],
+                ];
+            }//if($isModal)
+        }//if($params->safeGet('back_action',TRUE,'bool'))
     }//if(strlen($fTagId) && strlen($fResponseTarget))
 }//if($controlClass=='BasicForm' && isset($ctrl_params) && is_array($ctrl_params))
