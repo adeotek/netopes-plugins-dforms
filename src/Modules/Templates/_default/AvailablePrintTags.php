39f6709bc2921b5d2020f7da01776ce415dd0818
George Benjamin-Schonberger <george.benjamin@gmail.com>
2021-03-26 13:24:25 +0200

    Add available print tags in print template - DONE

--- src/Modules/Templates/_default/AvailablePrintTags.php
+++ src/Modules/Templates/_default/AvailablePrintTags.php
@@ -3,6 +3,10 @@ use NETopes\Core\Data\DataSet;
 
 /** @var \NETopes\Core\Data\DataSet|null $fields */
 /** @var \NETopes\Core\Data\DataSet|null $relations */
+/** @var string $targetTagId */
+/** @var string $targetTagType */
+
+$actionsDisabled=!strlen($targetTagId) || !strlen($targetTagType);
 
 if($fields instanceof DataSet && $fields->count()) {
     ?>
@@ -11,24 +15,25 @@ if($fields instanceof DataSet && $fields->count()) {
             <h4 style="margin: 0 0 5px 0;"><?= Translate::GetLabel('fields') ?></h4>
         </div>
         <div class="col-md-4">
-            <button class="btn btn-info btn-xs pull-right"><i class="fa fa-plus-circle" aria-hidden="true"></i><?= Translate::GetButton('add_all_fields') ?></button>
+            <button class="btn btn-info btn-xs pull-right" onclick="<?= NApp::Ajax()->Prepare("{ 'module': '{$this->name}', 'method': 'AddTagToPrintTemplate', 'params': { 'target_tag_id': '{$targetTagId}', 'target_tag_type': '{$targetTagType}', 'tags': '{nGet|df_fields_available_tags:form}' } }",'errors') ?>"<?= $actionsDisabled ? ' disabled="disabled"' : '' ?>><i class="fa fa-plus-circle" aria-hidden="true"></i><?= Translate::GetButton('add_all_fields') ?></button>
         </div>
     </div>
-    <div class="form-sm mt10 mb20">
+    <div class="form-sm mt10 mb20" id="df_fields_available_tags">
         <?php
         /** @var \NETopes\Core\Data\IEntity $field */
         foreach($fields as $field) {
             if(in_array($field->getProperty('class'),['FormTitle','FormSubTitle','FormSeparator','Message'])) {
                 continue;
             }
+            $fieldUid=$field->getProperty('uid');
             ?>
             <div class="row">
                 <div class="form-group col-md-6">
                     <div class="input-group input-group-sm">
                 <span class="input-group-btn">
-                    <button class="btn btn-primary"><i class="fa fa-plus" aria-hidden="true"></i><?= Translate::GetButton('add') ?></button>
+                    <button class="btn btn-primary" onclick="<?= NApp::Ajax()->Prepare("{ 'module': '{$this->name}', 'method': 'AddTagToPrintTemplate', 'params': { 'target_tag_id': '{$targetTagId}', 'target_tag_type': '{$targetTagType}', 'tag': '{nGet|dff-tag-{$fieldUid}:value}' } }",'errors') ?>"<?= $actionsDisabled ? ' disabled="disabled"' : '' ?>><i class="fa fa-plus" aria-hidden="true"></i><?= Translate::GetButton('add') ?></button>
                 </span>
-                        <input type="text" class="clsTextBox form-control text-center" readonly="readonly" value="[[<?= $field->getProperty('name') ?>]]">
+                        <input type="text" id="dff-tag-<?= $fieldUid ?>" name="<?= $fieldUid ?>" class="clsTextBox form-control text-center postable" readonly="readonly" value="[[<?= $field->getProperty('name') ?>]]">
                     </div>
                 </div>
                 <div class="form-group col-md-6">
@@ -50,10 +55,10 @@ if($relations instanceof DataSet && $fields->count()) {
             <h4 style="margin: 0 0 5px 0;"><?= Translate::GetLabel('relations') ?></h4>
         </div>
         <div class="col-md-4">
-            <button class="btn btn-info btn-xs pull-right"><i class="fa fa-plus-circle" aria-hidden="true"></i><?= Translate::GetButton('add_all_relations') ?></button>
+            <button class="btn btn-info btn-xs pull-right" onclick="<?= NApp::Ajax()->Prepare("{ 'module': '{$this->name}', 'method': 'AddTagToPrintTemplate', 'params': { 'target_tag_id': '{$targetTagId}', 'target_tag_type': '{$targetTagType}', 'tags': '{nGet|df_relations_available_tags:form}' } }",'errors') ?>"<?= $actionsDisabled ? ' disabled="disabled"' : '' ?>><i class="fa fa-plus-circle" aria-hidden="true"></i><?= Translate::GetButton('add_all_relations') ?></button>
         </div>
     </div>
-    <div class="form-sm mt10 mb20">
+    <div class="form-sm mt10 mb20" id="df_relations_available_tags">
         <?php
         /** @var \NETopes\Core\Data\IEntity $relation */
         foreach($relations as $relation) {
@@ -62,6 +67,7 @@ if($relations instanceof DataSet && $fields->count()) {
             if(!count($displayFields)) {
                 continue;
             }
+            $relationUid=$relation->getProperty('id');
             ?>
             <div class="row mb10">
                 <div class="col-12">
@@ -73,14 +79,15 @@ if($relations instanceof DataSet && $fields->count()) {
                             continue;
                         }
                         $tag=$relation->getProperty('key').'-'.strtolower($field);
+                        $fieldUid=$relationUid.'-'.strtoupper($field);
                         ?>
                         <div class="row">
                             <div class="form-group col-md-6">
                                 <div class="input-group input-group-sm">
                         <span class="input-group-btn">
-                            <button class="btn btn-primary"><i class="fa fa-plus" aria-hidden="true"></i><?= Translate::GetButton('add') ?></button>
+                            <button class="btn btn-primary" onclick="<?= NApp::Ajax()->Prepare("{ 'module': '{$this->name}', 'method': 'AddTagToPrintTemplate', 'params': { 'target_tag_id': '{$targetTagId}', 'target_tag_type': '{$targetTagType}', 'tag': '{nGet|dfr-tag-{$fieldUid}:value}' } }",'errors') ?>"<?= $actionsDisabled ? ' disabled="disabled"' : '' ?>><i class="fa fa-plus" aria-hidden="true"></i><?= Translate::GetButton('add') ?></button>
                         </span>
-                                    <input type="text" class="clsTextBox form-control text-center" readonly="readonly" value="[[<?= $tag ?>]]">
+                                    <input type="text" id="dfr-tag-<?= $fieldUid ?>" name="<?= $fieldUid ?>" class="clsTextBox form-control text-center postable" readonly="readonly" value="[[<?= $tag ?>]]">
                                 </div>
                             </div>
                             <div class="form-group col-md-6">
